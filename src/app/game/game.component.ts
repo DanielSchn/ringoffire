e2bad66fd5a7c65b1f063bd71ea6949ce94ee0ba
DanielSchn <d.schneider@dsc-mail.de>
2024-02-20 16:54:40 +0100

    Service class

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+// import { Game } from './../models/game';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-game',
@@ -8,8 +10,21 @@ import { Component } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
   pickCardAnimation = false;
+  // game!: Game;
+  game = inject(GameService);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new GameService();
+    console.log(this.game);
+  }
 
   takeCard() {
     this.pickCardAnimation = true;
