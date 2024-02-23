13ebd361e630fa7b0ab35e37c8b9ef9cc83faee2
DanielSchn <d.schneider@dsc-mail.de>
2024-02-23 11:41:20 +0100

    add rule card

---
+++ src/app/rule-card/rule-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rule-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './rule-card.component.html',
+  styleUrl: './rule-card.component.scss'
+})
+export class RuleCardComponent {
+
+}
